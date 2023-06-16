8ff4239a86d2609cadd443ca52ecd1fd30ee7e55
mikemikky762 <mikemikky762@gmail.com>
2023-06-15 22:09:41 -0700

    Thank God

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return:0 (Success)
+ */
+int main(void)
+{
+
+	char alx[] = " and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stdout, "%s\n", alx);
+	return (1);
+}
