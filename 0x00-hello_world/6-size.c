f8c3b05bc9509c663c0f79ee6e270f9ca1e63900
mikemikky762 <mikemikky762@gmail.com>
2023-06-15 21:12:36 -0700

    Getting there

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the size on various types on the computer it is compiled on
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", [unassigned long]sizeof[a]);
