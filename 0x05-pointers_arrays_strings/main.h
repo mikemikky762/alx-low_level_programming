1f39b080874fb7ca9541c16b1385b8e960010018
mikemikky762 <mikemikky762@gmail.com>
2023-06-26 20:19:11 -0700

    Print rev

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <unistd.h>
 
-int _putcha(char c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
