b4007fd5bc1bb8953ac5caa3fde1e4a3f2f6515c
mikemikky762 <mikemikky762@gmail.com>
2023-07-14 04:08:05 -0700

    Task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 #define ERR_MSG "Error"
@@ -55,6 +55,7 @@ int main(int argc, char *argv[])
 {
 char *s1, *s2;
 int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
