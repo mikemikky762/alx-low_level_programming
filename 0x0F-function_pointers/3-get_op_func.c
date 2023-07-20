60ad8f41eaa285175187dbe76cacb5da765830e7
mikemikky762 <mikemikky762@gmail.com>
2023-07-19 17:55:08 -0700

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,10 +1,12 @@
 #include "3-calc.h"
+#include <stdlib.h>
 /**
-*  * get_op_func - selects the correct function
-*   * to perform the operation asked by the user
-*    * @s: operator passed as argument to the program
-*     * Return: pointer to the function that corresponds
-*      * to the operator given as a parameter or NULL if not found
+*  * get_op_func - Selects the correct function to perform
+*   *               the operation asked by the user.
+*    * @s: The operator passed as argument.
+*     *
+*      * Return: A pointer to the function corresponding
+*       *         to the operator given as a parameter.
 */
 int (*get_op_func(char *s))(int, int)
 {
@@ -14,15 +16,13 @@ op_t ops[] = {
 {"*", op_mul},
 {"/", op_div},
 {"%", op_mod},
-{NULL, NULL}
+{NULL, NULL},
 };
+
 int i = 0;
 
-while (ops[i].op != NULL)
-{
-if (strcmp(s, ops[i].op) == 0)
-return (ops[i].f);
+while (ops[i].op != NULL && *(ops[i].op) != *s)
 i++;
-}
-return (NULL);
+
+return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,17 +4,11 @@
 #include "3-calc.h"
 /**
 *  * main - Prints the result of simple operations.
-*
 *   * @argc: The number of arguments supplied to the program.
-*
 *    * @argv: An array of pointers to the arguments.
-*
 *     *
-*
 *      * Return: Always 0.
-*
 */
-
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 int num1, num2;
@@ -25,6 +19,7 @@ if (argc != 4)
 printf("Error\n");
 exit(98);
 }
+
 num1 = atoi(argv[1]);
 op = argv[2];
 num2 = atoi(argv[3]);
@@ -41,7 +36,6 @@ if ((*op == '/' && num2 == 0) ||
 printf("Error\n");
 exit(100);
 }
-
 printf("%d\n", get_op_func(op)(num1, num2));
 return (0);
-
+}
